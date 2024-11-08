eaf2bb66406274fced1960b2992ce01ad5051ba3
Bruno Coelho <bruno-coelho@hotmail.com>
2024-11-08 00:50:19 -0300

    Inicio do 2 desafio. Criação do Menu inicial

---
+++ src/Main.js
@@ -0,0 +1,20 @@
+//import {MenuCadastroDePacientes} from './MenuCadastroDePacientes.js';
+//import {readlineSync} from './package.json'
+const myFunctions = require('./MenuCadastroDePacientes.js');
+
+console.log('\nMenu Principal');
+var  readlineSync  = require ( 'readline-sync' );
+  menuPrincipal  = ['Cadastro de pacientes ' , 'Agenda ' ];
+  index  = readlineSync.keyInSelect( menuPrincipal ,'Digite o comando desejado!', {cancel: 'Fim'});
+console.log ( 'Ok, vamos ao menu de ' +  (menuPrincipal [index] == undefined ? 'Fim' : menuPrincipal [index]) + '!');
+
+if (index == 0) {
+  console.log('digitou 1')
+  //console.table(menuPrincipal);
+  myFunctions.menuCadastroDePacientes();
+} else if (index == 1) {
+  console.log('digitou 2')
+  //MenuAgenda();
+} else {
+  console.log('digitou 0')
+}
---
+++ src/MenuCadastroDePacientes.js
@@ -0,0 +1,12 @@
+module.exports = {
+  menuCadastroDePacientes,
+}
+
+
+function menuCadastroDePacientes() {
+  console.log('Teste de função externa')
+};
+/*function menuCadastroDePacientes(a, b) {
+  return a + b
+};*/
+
